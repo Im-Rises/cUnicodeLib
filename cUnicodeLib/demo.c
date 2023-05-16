d91f22d485c3a1e34b8962b98f5f73a3d324a255
Im-Rises <quentin-morel88@hotmail.com>
2023-05-16 19:19:29 -0400

    wip

--- cUnicodeLib/demo.c
+++ cUnicodeLib/demo.c
@@ -5,15 +5,11 @@
 #include <Windows.h>
 #endif
 
-#ifdef _WIN32
-#define ANSI_COLOR_RED "\x1b[31m"
-#define ANSI_COLOR_BLUE "\x1b[44m"
-#define ANSI_COLOR_RESET "\x1b[0m"
-#else
-#define ANSI_COLOR_RED "\033[31m"
-#define ANSI_COLOR_BLUE "\033[44m"
-#define ANSI_COLOR_RESET "\033[0m"
-#endif
+#define PREFIX "\x1b["
+
+#define ANSI_COLOR_RED PREFIX "31m"
+#define ANSI_COLOR_BLUE PREFIX "34m"
+#define ANSI_COLOR_RESET PREFIX "0m"
 
 void initUnicodeLib() {
 #ifdef _WIN32
@@ -31,9 +27,9 @@ void initUnicodeLib() {
 const char* getAnsiColor(int colorCode) {
     static char code[10];
 #ifdef _WIN32
-    sprintf_s(code, sizeof(code), "\033[%dm", colorCode);
+    sprintf_s(code, sizeof(code), PREFIX "%dm", colorCode);
 #else
-    snprintf(code, sizeof(code), "\x1b[%dm", colorCode);
+    snprintf(code, sizeof(code), PREFIX "%dm", colorCode);
 #endif
     return code;
 }
@@ -56,14 +52,16 @@ int main() {
     printf_test_varargs(31, 44, "Printf test varargs кошка 日本国 %s, %d\n\n", "Windows", 10);
     printf("Test normal printf кошка 日本国\n\n");
 
-    // //     Test get Ansi Code
-    //    printf("%s Get Ansi code кошка 日本国\n\n", getAnsiColor(44));
+    //     Test get Ansi Code
+    printf("%s Get Ansi code кошка 日本国%s\n", getAnsiColor(92), getAnsiColor(0));
+    printf("кошка 日本国\n");
+    printf("%s Get Ansi code кошка 日本国%s\n", getAnsiColor(92), ANSI_COLOR_RESET);
+    printf("кошка 日本国\n");
 
-    // // Test
-    //    printf("кошка 日本国\n");
-    //    printf("%s Windows\n", ANSI_COLOR_RED);
-    //    printf("%s Windows%s\n", ANSI_COLOR_BLUE, ANSI_COLOR_RESET);
-    //    printf("%s Windows\n", ANSI_COLOR_RED);
+    // Test
+    printf("%s Windows\n", ANSI_COLOR_RED);
+    printf("%s Windows%s\n", ANSI_COLOR_BLUE, ANSI_COLOR_RESET);
+    printf("%s Windows\n", ANSI_COLOR_RED);
 
     return 0;
 }
