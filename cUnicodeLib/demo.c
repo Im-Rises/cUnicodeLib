2693211db8314ac2230c8ba454fce6584766e85c
Im-Rises <quentin-morel88@hotmail.com>
2023-05-16 17:40:31 -0400

    wip

--- cUnicodeLib/demo.c
+++ cUnicodeLib/demo.c
@@ -18,28 +18,21 @@
 
 #include "cUnicodeLib.h"
 
-int main()
-{
+int main() {
 #ifdef _WIN32
-//    // Working for UTF8 but no ANSI escape codes (colors)
-//    SetConsoleOutputCP(CP_UTF8);
-//    printf("кошка 日本国\n");
+    //    // Working for UTF8 but no ANSI escape codes (colors)
+    //    SetConsoleOutputCP(CP_UTF8);
+    //    printf("кошка 日本国\n");
 
-    // Configure console to enable ANSI escape codes
-//    initUnicodeLib();
-//    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-//    if (hOut == INVALID_HANDLE_VALUE) {
-//        return 1;
-//    }
-//    DWORD dwMode = 0;
-//    if (!GetConsoleMode(hOut, &dwMode)) {
-//        return 1;
-//    }
-//    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-//    if (!SetConsoleMode(hOut, dwMode)) {
-//        return 1;
-//    }
-//    _setmode(_fileno(stdout), _O_U8TEXT);
+    // Working for ANSI escape codes (colors) but no UTF8
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+    printf("кошка 日本国\n");
+
+
 
     printf("\x1b[44m Windows\n");
     printf("%sWindows%s\n", "\x1b[44m", "\x1b[0m");
@@ -54,44 +47,44 @@ int main()
     return 0;
 }
 
-//#ifdef _WIN32
-//#include <windows.h>
-//#else
-//#include <unistd.h>
-//#endif
+// #ifdef _WIN32
+// #include <windows.h>
+// #else
+// #include <unistd.h>
+// #endif
 //
-//#include <stdio.h>
+// #include <stdio.h>
 //
-//enum colors {
-//    BLACK,
-//    RED,
-//    GREEN,
-//    YELLOW,
-//    BLUE,
-//    MAGENTA,
-//    CYAN,
-//    WHITE
-//};
+// enum colors {
+//     BLACK,
+//     RED,
+//     GREEN,
+//     YELLOW,
+//     BLUE,
+//     MAGENTA,
+//     CYAN,
+//     WHITE
+// };
 //
-//void print_utf(enum colors foreground, enum colors background, const char* text) {
-//#ifdef _WIN32
-//    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-//    int color = foreground + (background * 16);
-//    SetConsoleTextAttribute(hConsole, color);
-//    printf("%s", text);
-//    SetConsoleTextAttribute(hConsole, 0x70);
-//#else
-//    printf("\033[0;%d;%dm%s\033[0m", 30 + foreground, 40 + background, text);
-//#endif
-//}
+// void print_utf(enum colors foreground, enum colors background, const char* text) {
+// #ifdef _WIN32
+//     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+//     int color = foreground + (background * 16);
+//     SetConsoleTextAttribute(hConsole, color);
+//     printf("%s", text);
+//     SetConsoleTextAttribute(hConsole, 0x70);
+// #else
+//     printf("\033[0;%d;%dm%s\033[0m", 30 + foreground, 40 + background, text);
+// #endif
+// }
 //
-//int main()
+// int main()
 //{
-//    print_utf(RED, 0, "My demo text\n");
-//    print_utf(BLUE, 0, "My demo text\n");
-//    print_utf(CYAN, 0, "My demo text\n");
-//    return 0;
-//}
+//     print_utf(RED, 0, "My demo text\n");
+//     print_utf(BLUE, 0, "My demo text\n");
+//     print_utf(CYAN, 0, "My demo text\n");
+//     return 0;
+// }
 //
 //
 //
