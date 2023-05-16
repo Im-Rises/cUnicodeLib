cb8589f23a1b02be1cdd3075c357200ba9e5ada7
Im-Rises <quentin-morel88@hotmail.com>
2023-05-16 11:52:17 -0400

    wip

--- cUnicodeLib/cUnicodeLib.c
+++ cUnicodeLib/cUnicodeLib.c
@@ -5,7 +5,7 @@ void initUnicodeLib()
 #ifdef _WIN32
 	if (!_setmode(_fileno(stdout), _O_U8TEXT))
 	{
-		printf("Cannot enable UTF16 in console.");
+		printf("Cannot enable UTF8 in console.");
 		exit(1);
 	}
 #endif
