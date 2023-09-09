c2e4588ed68bbc4537ab306c611a98e93cd287f6
Im-Rises <quentin-morel88@hotmail.com>
2023-09-09 14:26:24 -0400

    New version

--- test/main.c
+++ test/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "cUnicodeLib.h"
 
 int main() {
@@ -28,13 +29,13 @@ int main() {
     // Print utf8 text with changing colored foreground and colored background
     printf("%s%sPrint utf8 text with colored foreground %s%sand colored background: кошка 日本国%s\n", ESC_FG_B_BLUE, ESC_BG_B_RED, ESC_FG_RED, ESC_BG_BLUE, ESC_RESET_ALL);
 
-    // Print bold text
-    printf("%sBold: Bold text - %sNormal: Normal text\n", ESC_BOLD_ON, ESC_BOLD_FAINT_OFF);
-
-    /* Example with string concatenations */
-
-    // Print text with colored background and colored foreground
-    printf(ESC_BOLD_ON ESC_BG_B_WHITE ESC_FG_BLUE "Print text with colored background and colored foreground" ESC_RESET_ALL "\n");
+    //    // Print bold text
+    //    printf("%sBold: Bold text - %sNormal: Normal text\n", ESC_BOLD_ON, ESC_BOLD_FAINT_OFF);
+    //
+    //    /* Example with string concatenations */
+    //
+    //    // Print text with colored background and colored foreground
+    //    printf(ESC_BOLD_ON ESC_BG_B_WHITE ESC_FG_BLUE "Print text with colored background and colored foreground" ESC_RESET_ALL "\n");
 
     // Print utf8 text with colored background
     printf(ESC_BG_B_GREEN "Print utf8 text with colored background" ESC_RESET_ALL "\n");
